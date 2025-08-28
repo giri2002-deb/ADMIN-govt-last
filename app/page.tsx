b8f965581779289bdd03a3ec0a2da1a7fcfa8970
giri2002-deb <140328477+giri2002-deb@users.noreply.github.com>
2025-08-28 14:53:27 +0530

    Fix Netlify build: move BrowserRouter to client-only component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { AdminDashboard } from "@/components/admin-dashboard";
-import { BrowserRouter } from "react-router-dom";
 
 export default function Page() {
-  return (
-    <BrowserRouter>
-      <AdminDashboard />
-    </BrowserRouter>
-  );
+  return <AdminDashboard />;
 }
-
